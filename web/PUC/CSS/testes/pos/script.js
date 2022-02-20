679b96da08dbc1170369d2cdc1fda6917c8a9bda
Tamara Sposito <tamaragsposito@gmail.com>
2022-02-20 15:15:44 -0300

    first commit

---
+++ web/PUC/CSS/testes/pos/script.js
@@ -0,0 +1,7 @@
+function a {
+  document.getElementById('p1').innerHTML = Math.random();
+}
+/*function a {
+  
+  document.getElementById('p1').innerHTML = 'Altura: ${window.innerHeight} Largura: ${window.innerWidht}';
+}*/
